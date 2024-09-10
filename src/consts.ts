3975d030c4362e6cf417a09543275ac9fad683bb
TAYD3Y400 <taylor20sep.hc@gmail.com>
2024-09-09 19:37:18 -0600

    New Portafolio

---
+++ src/consts.ts
@@ -0,0 +1,134 @@
+import portafolioLogo from "./icons/thc-red.svg";
+import cometecLogo from "./icons/cometec.svg";
+import apolosLogo from "./icons/apolos.svg";
+import crevlogo from "./icons/crev.svg";
+
+export const SITE_TITLE = "THC";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const stack = [
+  {
+    name: "HTML",
+    color: "#EF652A",
+  },
+  {
+    name: "CSS",
+    color: "#30A9DC",
+  },
+  {
+    name: "JavaScript",
+    color: "#F0DB4F",
+  },
+  {
+    name: "TypeScript",
+    color: "#3178C6",
+  },
+  {
+    name: "Tailwind",
+    color: "#17BAB8",
+  },
+  {
+    name: "React",
+    color: "#00D8FF",
+  },
+  {
+    name: "Python",
+    color: "#DFAF22",
+  },
+  {
+    name: "Next",
+    color: "#FAFAFA",
+  },
+  {
+    name: "Astro",
+    color: "#FAFAFA",
+  },
+  {
+    name: "NodeJS",
+    color: "#539E43",
+  },
+  {
+    name: "ExpressJS",
+    color: "#FAFAFA",
+  },
+  {
+    name: "GraphQL",
+    color: "#F6009C",
+  },
+  {
+    name: "Postman",
+    color: "#FD5A11",
+  },
+  {
+    name: "MongoDB",
+    color: "#51ed45",
+  },
+  {
+    name: "Java",
+    color: "#6864e8",
+  },
+  {
+    name: "Docker",
+    color: "#3178C6",
+  },
+  {
+    name: "SQL",
+    color: "#8A4182",
+  },
+  {
+    name: "Jest",
+    color: "#99425B",
+  },
+  {
+    name: "Testing Library",
+    color: "#DC312F",
+  },
+  {
+    name: "Git",
+    color: "#DE4C36",
+  },
+  {
+    name: "Linux",
+    color: "#DFAF22",
+  },
+  {
+    name: "VS Code",
+    color: "#3DA9F2",
+  },
+  {
+    name: "Figma",
+    color: "#A95FFF",
+  },
+];
+
+
+export const projects = [
+  {
+    name: "ComeTEC",
+    description:
+    'A light, user-friendly web application designed to host <b style="color: #C70039;">nationwide</b> inter-school math competitions. Provides an intuitive platform for students and schools to participate in and manage <b style="color: #C70039;">mathematical</b> challenges, fostering academic excellence. The app is built to handle <b style="color: #C70039;">real-time scoring</b>, ranking and result publishing, aimed at promoting educational growth.',
+    image: cometecLogo,
+    url: "https://github.com/honojs/middleware/tree/main/packages/oauth-providers",
+  }, 
+  {
+    name: "CREV Website",
+    description:
+    'A clean and intuitive <b style="color: #C70039;">catalog</b> and information website for <b style="color: #C70039;">CREV</b>, providing users with detailed information on products, services, and company updates. Designed for easy navigation and a smooth user experience, allowing visitors to quickly find what they need.',
+    image: crevlogo,
+    url: "https://crevlatam.com/?page_id=1171",
+  },
+  {
+    name: "Apolos Library Website",
+    description:
+    'A sleek and easy-to-use <b style="color: #C70039;">eCommerce</b> platform designed for buying and selling books online. Users can easily <b style="color: #C70039;">register</b> for accounts, browse a wide collection of books, and securely <b style="color: #C70039;">purchase</b> their favorites.',
+    image: apolosLogo,
+    url: "https://paleta-v3.vercel.app/",
+  },
+  {
+    name: "My Portafolio",
+    description:
+    'A personal <b style="color: #C70039;">portfolio</b> showcasing my journey as a developer, featuring an <b style="color: #C70039;">About Me</b> section, detailed <b style="color: #C70039;">experience</b>, selected <b style="color: #C70039;">projects</b>, tech <b style="color: #C70039;">stack</b>, and <b style="color: #C70039;">educational background</b>. Designed to highlight my skills, growth, and professional achievements.',
+    image: portafolioLogo,
+    url: "/colors-kit-docs",
+  },
+];
